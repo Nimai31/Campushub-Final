b8a8cd2754841841e01d36eb1943c8703c2b6ef6
Nimai31 <nimaipoleplli@gmail.com>
2024-05-25 09:33:49 +0530

    first

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,24 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMqXxbDkhHFhbZ_XFa3Z0XJz_x_hEg27M",
-  authDomain: "campushub-9f877.firebaseapp.com",
-  projectId: "campushub-9f877",
-  storageBucket: "campushub-9f877.appspot.com",
-  messagingSenderId: "26619626465",
-  appId: "1:26619626465:web:90060dd89f61c31ceb48a0",
+  apiKey: "AIzaSyClvkvGNDw2YtNuVUz7lMo2bgaWs4jJ-D8",
+  authDomain: "campushub-d98bd.firebaseapp.com",
+  projectId: "campushub-d98bd",
+  storageBucket: "campushub-d98bd.appspot.com",
+  messagingSenderId: "643264223185",
+  appId: "1:643264223185:web:f2d86a18930fdd0f3e32f0",
 };
-
-//Viswantahs login
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBOP21xXjFC_FClCf0A9e1Z4F5BwiAmpow",
-//   authDomain: "campushub-1c470.firebaseapp.com",
-//   projectId: "campushub-1c470",
-//   storageBucket: "campushub-1c470.appspot.com",
-//   messagingSenderId: "985073052328",
-//   appId: "1:985073052328:web:6f7cc0e0e2001260aeb09d",
-//   measurementId: "G-W3XZWWGLME",
-// };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();

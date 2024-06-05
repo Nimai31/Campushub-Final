bf15a8eea42be17fd956516de15d046ac20bf01b
Nimai31 <nimaipoleplli@gmail.com>
2024-06-05 21:14:35 +0530

    change made

--- src/components/ProjectCollab.jsx
+++ src/components/ProjectCollab.jsx
@@ -109,7 +109,7 @@ const ProjectCollab = (props) => {
     <Container>
       <ProjectBox>
         <button onClick={toggleProjectForm}>
-          Create Project
+          Create a Project
         </button>
         <Modal show={showProjectForm} onClose={resetForm}>
           <ProjectForm>
